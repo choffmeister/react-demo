a388b48959900b8fb8ce89c9110a4d5af4a2e762
Christian Hoffmeister <mail@choffmeister.de>
2015-01-17 09:54:28 +0100

    Temp

---
+++ README.md
@@ -0,0 +1,12 @@
+# react-demo
+
+~~~ bash
+# initialize
+$ npm install
+
+# run in development mode
+$ gulp --watch
+
+# build distributable package
+$ gulp build --dist
+~~~
--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,59 @@
 var argv = require('yargs').argv,
-    gulp = require('gulp'),
-    browserify = require('gulp-browserify'),
-    rename = require('gulp-rename'),
+    browserify = require('browserify'),
+    buffer = require('vinyl-buffer'),
     gif = require('gulp-if'),
-    uglify = require('gulp-uglify');
+    gulp = require('gulp'),
+    gutil = require('gulp-util'),
+    minifyhtml = require('gulp-minify-html'),
+    rename = require('gulp-rename'),
+    rimraf = require('gulp-rimraf'),
+    source = require('vinyl-source-stream'),
+    sourcemaps = require('gulp-sourcemaps'),
+    uglify = require('gulp-uglify'),
+    watchify = require('watchify');
 
 var config = {
   debug: !argv.dist,
-  dist: argv.dist
+  dist: argv.dist,
+  watch: argv.watch
 };
 
-gulp.task('javascript', function () {
-  gulp.src('./index.js', { read: false })
-    .pipe(browserify({
-      transform: [],
-      extensions: []
-    }))
-    .pipe(gif(config.dist, uglify({ preserveComments: 'some' })))
-    .pipe(rename('app.js'))
-    .pipe(gulp.dest('./target/app'));
+gulp.task('html', ['clean'], function () {
+  return gulp.src('./index.html')
+    .pipe(gif(config.dist, minifyhtml()))
+    .pipe(gulp.dest('./target'));
 });
 
-gulp.task('default', ['javascript']);
+gulp.task('javascript', ['clean'], function () {
+  var inst = bundler();
+  return bundle();
+
+  function bundler() {
+    var b = browserify('./index.js');
+    if (config.watch) {
+      var w = watchify(b, watchify.args);
+      w.on('update', bundle);
+      return w;
+    } else {
+      return b;
+    }
+  };
+
+  function bundle() {
+    return inst.bundle()
+      .pipe(source('app.js'))
+      .pipe(buffer())
+      .pipe(gif(config.debug, sourcemaps.init({ localMaps: true })))
+      .pipe(gif(config.dist, uglify({ preserveComments: 'some' })))
+      .pipe(sourcemaps.write('.'))
+      .pipe(gulp.dest('./target/app'));
+  };
+});
+
+gulp.task('clean', function () {
+  return gulp.src('./target', { read: false })
+    .pipe(rimraf());
+})
+
+gulp.task('build', ['html', 'javascript'])
+gulp.task('default', ['build']);
