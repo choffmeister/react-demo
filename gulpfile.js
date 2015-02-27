1d0240eaf7fc779a1002d91c933751440c841a82
Christian Hoffmeister <mail@choffmeister.de>
2015-02-27 05:34:49 +0100

    Refactor gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -13,48 +13,48 @@ var argv = require('yargs').argv,
     size = require('gulp-size'),
     source = require('vinyl-source-stream'),
     sourcemaps = require('gulp-sourcemaps'),
-    uglify = require('gulp-uglify'),
-    watchify = require('watchify');
+    uglify = require('gulp-uglify');
 
 var config = {
   debug: !argv.dist,
   dist: argv.dist,
-  watch: argv.watch,
   port: argv.port || 9000
 };
 
-gulp.task('html', ['clean'], function () {
+var onerror = function (err) {
+  if (config.debug) {
+    gutil.beep();
+    gutil.log(err.message);
+  } else {
+    throw err;
+  }
+};
+
+gulp.task('html', function () {
   return gulp.src('./index.html')
     .pipe(gif(config.dist, minifyhtml()))
     .pipe(size({ showFiles: true, gzip: config.dist }))
+    .pipe(connect.reload())
     .pipe(gulp.dest('./target'));
 });
 
-gulp.task('css', ['clean'], function () {
+gulp.task('css', function () {
   return gulp.src('./app/style.less')
     .pipe(less({ compress: config.dist }))
+    .on('error', onerror)
     .pipe(size({ showFiles: true, gzip: config.dist }))
+    .pipe(connect.reload())
     .pipe(gulp.dest('./target/app'))
 });
 
-gulp.task('javascript', ['clean'], function () {
-  var inst = bundler();
+gulp.task('javascript', function () {
+  var bundler = browserify('./app/app.jsx')
+    .transform(reactify);
   return bundle();
 
-  function bundler() {
-    var b = browserify('./app/app.jsx')
-      .transform(reactify);
-    if (config.watch) {
-      var w = watchify(b, watchify.args);
-      w.on('update', bundle);
-      return w;
-    } else {
-      return b;
-    }
-  };
-
   function bundle() {
-    return inst.bundle()
+    return bundler.bundle()
+      .on('error', onerror)
       .pipe(source('app.js'))
       .pipe(buffer())
       .pipe(gif(config.debug, sourcemaps.init({ localMaps: true })))
@@ -70,13 +70,16 @@ gulp.task('connect', ['build'], function () {
   connect.server({
     port: config.port,
     root: './target',
-    livereload: config.watch
+    livereload: true
   });
 });
 
-gulp.task('clean', function (cb) {
-  del(['./target/**'], cb);
-})
+gulp.task('watch', ['build'], function () {
+  gulp.watch('./index.html', ['html']);
+  gulp.watch('./app/**/*.{css,less}', ['css']);
+  gulp.watch('./app/**/*.{js,jsx}', ['javascript']);
+});
 
 gulp.task('build', ['html', 'css', 'javascript'])
-gulp.task('server', ['connect']);
+gulp.task('server', ['connect', 'watch']);
+gulp.task('default', ['server']);
